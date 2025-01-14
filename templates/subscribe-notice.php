10aa7146b3cf6c9cb0c7da6b449aa4e03f23e7d2
Christos Athanasiadis <chris.k.athanasiadis@gmail.com>
2025-01-14 10:30:07 +0000

    chore: Release patch (4.0.9)

--- templates/subscribe-notice.php
+++ templates/subscribe-notice.php
@@ -11,14 +11,14 @@
  */
 ?>
 <div id="ms-subscribe-notice-container"
-    class="notice woocommerce-message woocommerce-admin-promo-messages is-dismissible ms-hidden">
+     class="notice woocommerce-message woocommerce-admin-promo-messages is-dismissible ms-hidden">
     <p>📊 <strong>Sort</strong>
     <div>
-        <div style="margin-bottom: 10px;"><?php echo  $message ?></div>
-        <input type="hidden" value="<?php echo  $siteUrl ?>" id="msSiteUrl" name="msSiteUrl">
+        <div style="margin-bottom: 10px;"><?php echo $message?></div>
+        <input type="hidden" value="<?php echo $siteUrl?>" id="msSiteUrl" name="msSiteUrl">
         <form id="ms-subscribe-form">
-            <input type="email" value="<?php echo  $adminEmail ?>" size="<?php echo  strlen($adminEmail) ?>" id="msAdminEmail"
-                name="msAdminEmail" required />
+            <input type="email" value="<?php echo $adminEmail?>" size="<?php echo strlen($adminEmail)?>" id="msAdminEmail"
+                   name="msAdminEmail" required/>
             <input type="submit" value="Submit" class="button button-primary">
         </form>
     </div>
